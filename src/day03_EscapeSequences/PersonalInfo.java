959d49d9e46f780d55bf129df99d5fde3e11073e
Nazir <nmurtuzayev@gmail.com>
2022-01-23 21:21:06 -0500

    Commit2

--- src/day03_EscapeSequences/PersonalInfo.java
+++ src/day03_EscapeSequences/PersonalInfo.java
@@ -4,7 +4,7 @@ public class PersonalInfo {
 
     public static void main(String[] args) {
 
-        System.out.println("Name: John");
+        System.out.println("Name: Nazir");
         System.out.println("Gender: Male");
         System.out.println("Date of Birth: 06/19/1993");
         System.out.println("Address: Los Angeles, CA");
