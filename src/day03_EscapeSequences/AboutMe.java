36e82c4d9241c11cb53dd30d19dcc27b9edccb6d
Nazir <nmurtuzayev@gmail.com>
2022-01-23 21:19:57 -0500

    Commit1

--- src/day03_EscapeSequences/AboutMe.java
+++ src/day03_EscapeSequences/AboutMe.java
@@ -6,7 +6,7 @@ public class AboutMe {
     public static void main(String[] args) {
 
 
-        System.out.println("\tMy favorite music is\"Rock\" musics. \n\tMy favorite book is \"Data Structure in Java\". \n\tMy favorite Tv show is \"Peaky Blinders\". \n\tMy favorite Quote is \" The greatness of richness is richness of the soul\". ");
+        System.out.println("\tMy favorite music is musics. \n\tMy favorite book is \"Data Structure in Java\". \n\tMy favorite Tv show is \"Peaky Blinders\". \n\tMy favorite Quote is \" The greatness of richness is richness of the soul\". ");
 
 
 
